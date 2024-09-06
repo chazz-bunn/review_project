8cdd26b2aa2f10e99248c4595073ac2d72090ddc
chazz-bunn <darrylcbunn@gmail.com>
2024-09-06 11:17:13 -0500

    improvements

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ spark = SparkSession.builder.master("local").appName("Example1").getOrCreate()
 sc = spark.sparkContext
 sc.setLogLevel("WARN")
 
-#Login into mysql and sakila database, and setp cursor and connection objects
+### MUST have file mysqllogininfo.json file with the following:
 '''
-Must have file mysqllogininfo.json file with:
 {
     "user":"[username]",
     "password":"[password]",
@@ -17,13 +16,14 @@ Must have file mysqllogininfo.json file with:
 }
 '''
 with open("mysqllogininfo.json") as jsonfile:
-        login_dict = json.load(jsonfile)
+     login_dict = json.load(jsonfile)
 
+# Set up cursor and connection objects
 cnx = mysql.connector.connect(
-    user=login_dict["user"], 
-    password=login_dict["password"], 
-    host=login_dict["host"], 
-    database="sakila"
+     user=login_dict["user"], 
+     password=login_dict["password"], 
+     host=login_dict["host"], 
+     database="sakila"
 )
 cursor = cnx.cursor()
 
@@ -39,8 +39,8 @@ def change_type(x):
 # Function for converting a table to a dataframe
 def table_to_df(table_name:str):
     cursor.execute(f"DESCRIBE {table_name};")
-    results = cursor.fetchall()
-    columns = [r[0] for r in results]
+    descriptor = cursor.fetchall()
+    columns = [r[0] for r in descriptor]
     cursor.execute(f"SELECT * FROM {table_name};")
     results = []
     while True:
@@ -62,18 +62,22 @@ def main():
     q1_df = actor_df.dropDuplicates(["last_name"]).agg({'last_name': 'count'})
     print("\nHow many distinct actors last names are there?")
     q1_df.show(truncate = False)
+
     # 2. Which last names are not repeated?
     q2_df = actor_df.groupBy(["last_name"]).count().filter("count = 1").select(["last_name"])
     print("Which last names are not repeated?")
     q2_df.show(q2_df.count(), truncate = False)
+
     # 3. Which last names appear more than once?
     q3_df = actor_df.groupBy(["last_name"]).count().filter("count > 1").select(["last_name"])
     print("Which last names appear more than once?")
     q3_df.show(q3_df.count(), truncate = False)
+
     # 4. What is that average running time of all the films in the sakila DB?
     q4_df = film_df.agg({'length': 'avg'})
     print("What is that average running time of all the films in the sakila DB?")
     q4_df.show(truncate = False)
+    
     # 5. What is the average running time of films by category?
     q5_df = film_df.join(film_category_df, film_df.film_id == film_category_df.film_id)\
         .join(category_df, film_category_df.category_id == category_df.category_id)\
